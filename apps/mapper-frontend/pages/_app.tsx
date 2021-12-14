f861db9c49b7b1424c5a8b6e72b1849cee81de94
David Corson <david.l.corson@gmail.com>
2021-12-13 22:53:26 -0500

    removing boilerplate from nextjs and root

--- apps/mapper-frontend/pages/_app.tsx
+++ apps/mapper-frontend/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to mapper-frontend!</title>
+        <title>Mapper</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
